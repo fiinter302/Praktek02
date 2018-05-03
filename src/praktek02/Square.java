feae4e7af4b8ec4a98467a7ff78d74f3a7141d79
Mukhamad Syafii <fiiinter302@gmail.com>
2018-05-03 22:10:15 +0700

    membuat Attribut dan Behavior

--- src/praktek02/Square.java
+++ src/praktek02/Square.java
@@ -1,4 +1,10 @@
 package praktek02;
 public class Square {
+    double Sisi;
     
+    void cetakifo(){
+        System.out.println("===========");
+        System.out.println("Sisi+ "+Sisi);
+        System.out.println("===========");
+    }
 }
